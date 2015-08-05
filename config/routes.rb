30967ebfd28fdaf26665069bee5aae267be222f8
Josh Cass <joshuacass@gmail.com>
2015-08-04 20:01:34 -0600

    Added Omniauth Twitter login
    * created User model to handle user creation with login
    
    * added authorization filter for dashboard and future controllers:

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
+  get '/auth/twitter', as: 'login'
+  get '/auth/twitter/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   resources :dashboard, only: [:index]
 end
