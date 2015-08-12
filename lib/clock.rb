562ed5971c9a0b541c5ff8e3bc35f201bfa5f667
Josh Cass <joshuacass@gmail.com>
2015-08-11 21:17:47 -0600

    Changed the clockwork schedule back to something sane

--- lib/clock.rb
+++ lib/clock.rb
@@ -3,5 +3,5 @@ require './config/boot'
 require './config/environment'
 
 module Clockwork
-  every(10.seconds, 'feed_schedule') { Resque.enqueue(FeedScheduleJob) }
+  every(5.minutes, 'feed_schedule') { Resque.enqueue(FeedScheduleJob) }
 end
